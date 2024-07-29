bd21c61218cc8a894b35d5e7ef7d746db7042466
CassioJhones <cassio.bjhones@gmail.com>
2024-07-29 00:38:09 -0300

    novaPagina de atividades

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,3 +1,95 @@
-export function TripDetailsPage(){
-  return <h1>PAGINA DE DETALHES</h1>
+import { ArrowRight, Calendar, CircleCheck, Link2, MapPin, Plus, Settings2 } from "lucide-react";
+
+export function TripDetailsPage() {
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-centern justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">Rio Claro</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-zinc-100">16 a 20 de Outubro</span>
+          </div>
+
+          <div className='w-px h-6 bg-zinc-800' />
+
+          <button className='bg-zinc-800 text-zinc-200 rounded-lg px-3 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+            Alterar local/data
+            <Settings2 className='size-5' />
+          </button>
+        </div>
+      </div>
+
+      <main className="flex gap-16 px-4">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+              <Plus className='size-5' />
+              Cadastrar Atividades
+            </button>
+          </div>
+
+          <div className="space-y-8">
+            <div className="space-y-2.5">
+              <div className=" flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p className="text-zinc-500 text-sm">Nenhuma atividade encontrada nessa data.</p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className=" flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em Grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00</span>
+                </div>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Almoço em Grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00</span>
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+        </div>
+
+        <div className="w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links Importantes</h2>
+            <div className="space-y-5">
+              <div className="fllex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do AirbNb</span>
+                  <span className="block text-xs text-zinc-400 truncate">https://www.airbnb.com.br/rooms/6541894651651964865135196546as1d3a2165461as32d1as56113a1sd9</span>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0" />
+              </div>
+            </div>
+            <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg  px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+              <Plus className='size-5' /> Cadastrar Novo Link </button>
+          </div>
+          <div className='w-px h-6 bg-zinc-800' />
+          <div></div>
+
+        </div>
+      </main>
+    </div>
+  )
 }
