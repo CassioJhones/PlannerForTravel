30fab9eee10802be780470239d32d0d7b821bfd6
CassioJhones <cassio.bjhones@gmail.com>
2024-08-06 19:28:11 -0300

    post atividades

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -7,7 +7,7 @@ import { ptBR } from "date-fns/locale";
 
 interface Activity {
   date: string
-  acitivies: {
+  activities: {
     id: string
     title: string
     occurs_at: string
@@ -25,28 +25,34 @@ export function Activities() {
   return (
     <div className="space-y-8">
 
-      {activities.map(atividade => {
+      {activities.map(category => {
         return (
-          <div className="space-y-2.5">
+          <div key={category.date} className="space-y-2.5">
             <div className=" flex gap-2 items-baseline">
-              <span className="text-xl text-zinc-300 font-semibold">Dia {format(atividade.date, 'd')}</span>
-              <span className="text-xs text-zinc-500">{format(atividade.date, 'EEEE',{locale:ptBR})}</span>
+              <span className="text-xl text-zinc-300 font-semibold">Dia {format(category.date, 'd')}</span>
+              <span className="text-xs text-zinc-500">{format(category.date, 'EEEE', { locale: ptBR })}</span>
             </div>
-            {atividade.acitivies.length>0
-            ?(
-              <div></div>
-            )
-            :(<p className="text-zinc-500 text-sm">Nenhuma atividade encontrada nessa data.</p>)
-            }
-            
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map(x => {
+                  return (
+                    <div key={x.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex gap-3 items-center">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{x.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(x.occurs_at, 'HH:mm', { locale: ptBR })}
+                        </span>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+            ) : (<p className="text-zinc-500 text-sm">Nenhuma atividade encontrada nessa data.</p>
+            )}
           </div>
-
         )
       })}
-
-
-
-      
     </div>
 
   )
